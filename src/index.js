3e0f018268e2e38aef061a167264df95bee683dc
pomdtr <achille.lacoin@gmail.com>
2023-01-01 13:02:47 +0100

    MVP

---
+++ src/index.js
@@ -0,0 +1,192 @@
+const {
+  app,
+  BrowserWindow,
+  globalShortcut,
+  screen,
+  ipcMain,
+  shell,
+  clipboard,
+} = require("electron");
+const path = require("path");
+const url = require("url");
+const pty = require("node-pty");
+const os = require("os");
+
+process.env.ELECTRON_DISABLE_SECURITY_WARNINGS = "true";
+
+const isSingleInstance = app.requestSingleInstanceLock();
+if (!isSingleInstance) {
+  app.quit();
+  process.exit(0);
+}
+
+app.on("window-all-closed", () => {
+  app.quit();
+});
+
+function createWindow() {
+  const bounds = getCenterOnCurrentScreen();
+  const win = new BrowserWindow({
+    title: "Sunbeam",
+    width: 750,
+    height: 475,
+    frame: false,
+    x: bounds.x,
+    y: bounds.y,
+    alwaysOnTop: true,
+    skipTaskbar: true,
+    minimizable: false,
+    transparent: true,
+    maximizable: false,
+    vibrancy: "under-window",
+    fullscreenable: false,
+    movable: false,
+    autoHideMenuBar: true,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+    resizable: false,
+    type: "panel",
+    show: false,
+    hasShadow: true,
+  });
+  win.setMenu(null);
+  win.loadFile(path.join(__dirname, "index.html"));
+  ipcMain.handle("hideWindow", () => win.hide());
+  ipcMain.handle("showWindow", () => win.show());
+
+  ipcMain.handle("copyToClipboard", (_, text) => {
+    clipboard.writeText(text);
+  });
+  ipcMain.handle("openInBrowser", (_, url) => {
+    return shell.openExternal(url);
+  });
+  ipcMain.handle("open", (_, path) => {
+    return shell.openPath(path);
+  });
+
+  win.webContents.setWindowOpenHandler(({ url }) => {
+    shell.openExternal(url);
+    return { action: "deny" };
+  });
+
+  win.webContents.on("did-start-loading", () => {
+    win.hide();
+  });
+
+  win.on("blur", () => {
+    win.hide();
+  });
+
+  return win;
+}
+
+const getCenterOnCurrentScreen = () => {
+  const cursor = screen.getCursorScreenPoint();
+  // Get display with cursor
+  const distScreen = screen.getDisplayNearestPoint({
+    x: cursor.x,
+    y: cursor.y,
+  });
+
+  const { width: screenWidth, height: screenHeight } = distScreen.workAreaSize;
+  const width = 750;
+  const height = 475;
+  const x = distScreen.workArea.x + Math.floor(screenWidth / 2 - width / 2); // * distScreen.scaleFactor
+  const y = distScreen.workArea.y + Math.floor(screenHeight / 2 - height / 2);
+
+  return {
+    width,
+    height,
+    x,
+    y,
+  };
+};
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+app.on("window-all-closed", () => {
+  // pass
+});
+app.setAsDefaultProtocolClient("sunbeam");
+if (process.platform === "darwin") {
+  app.dock.hide();
+}
+
+app.whenReady().then(async () => {
+  const win = createWindow();
+  win.webContents.on("dom-ready", async () => {
+    globalShortcut.register("CommandOrControl+;", async () => {
+      if (win.isVisible()) {
+        win.hide();
+      } else {
+        const bounds = getCenterOnCurrentScreen();
+        if (JSON.stringify(bounds) !== JSON.stringify(win.getBounds())) {
+          win.setBounds(bounds);
+          await sleep(50);
+        }
+        win.show();
+      }
+    });
+    while (true) {
+      await runSunbeam(win);
+      win.hide();
+    }
+  });
+
+  app.on("open-url", (_, sunbeamUrl) => {
+    const parsedUrl = url.parse(sunbeamUrl);
+    switch (parsedUrl.host) {
+      case "run":
+        win.loadFile(MAIN_WINDOW_WEBPACK_ENTRY);
+    }
+  });
+});
+
+function runSunbeam(win) {
+  return new Promise((resolve, reject) => {
+    const disposables = [];
+    const sunbeam = app.isPackaged
+      ? path.join(process.resourcesPath, "sunbeam")
+      : "sunbeam";
+
+    const { shell } = os.userInfo();
+
+    const ptyProcess = pty.spawn(shell, ["-c", sunbeam], {
+      env: {
+        ...process.env,
+        TERM: "xterm-256color",
+      },
+    });
+
+    disposables.push(
+      ptyProcess.onData((data) => {
+        win.webContents.send("pty-output", data);
+      })
+    );
+
+    ipcMain.on("pty-resize", (_, columns, rows) => {
+      ptyProcess.resize(columns, rows);
+    });
+
+    ipcMain.on("pty-input", (_, data) => {
+      ptyProcess.write(data);
+    });
+
+    win.webContents.send("pty-ready");
+
+    ptyProcess.onExit(({ exitCode }) => {
+      ipcMain.removeAllListeners("pty-resize");
+      ipcMain.removeAllListeners("pty-input");
+      disposables.forEach((d) => d.dispose());
+
+      if (exitCode === 0) {
+        resolve();
+      } else {
+        reject(new Error(`Sunbeam exited with code ${exitCode}`));
+      }
+    });
+  });
+}
---
+++ src/preload.js
@@ -0,0 +1,15 @@
+// See the Electron documentation for details on how to use preload scripts:
+// https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+
+const { contextBridge, ipcRenderer } = require("electron");
+
+contextBridge.exposeInMainWorld("pty", {
+  write: (data) => ipcRenderer.send("pty-input", data),
+  onData: (callback) => {
+    ipcRenderer.on("pty-output", (_, data) => callback(data));
+  },
+  onReady: (callback) => {
+    ipcRenderer.on("pty-ready", (_, data) => callback(data));
+  },
+  resize: (columns, rows) => ipcRenderer.send("pty-resize", columns, rows),
+});
